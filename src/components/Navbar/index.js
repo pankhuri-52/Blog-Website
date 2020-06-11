5d35fa87d6f1f4d016a690648ae6ac05c196c579
Pankhuri <pankhuritrikha@gmail.com>
2020-06-11 14:18:37 +0530

    Setting up of navigation bar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+
+const Navbar = (props) => {
+    return (
+        <div className="navbar">
+           <ul className="navbarMenu">
+             <li><a href="#">Home</a></li>
+             <li><a href="#">About Us</a></li>
+             <li><a href="#">Posts</a></li>
+             <li><a href="#">Contact Us</a></li>
+           </ul>
+        </div>
+    );
+}
+export default Navbar;
