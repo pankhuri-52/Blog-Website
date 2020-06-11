e7ee0f9f50aedc9cc96ed3845b0b271b9b263a6f
Pankhuri <pankhuritrikha@gmail.com>
2020-06-11 12:24:48 +0530

    Changes in the header

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => {
+    return (
+        <header>
+            <nav>
+                <a href="#">Home</a>
+                <a href="#">About Us</a>
+                <a href="#">Contact Us</a>
+            </nav>
+        </header>
+    );
+}
+export default Header;
