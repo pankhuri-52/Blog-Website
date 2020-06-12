8b17cfd327a4bab7c519e7a6744ba4577b037621
Pankhuri <pankhuritrikha@gmail.com>
2020-06-12 12:42:23 +0530

    Post section is done

--- src/components/blogPost/index.js
+++ src/components/blogPost/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './style.css';
+import Card from '../UI/Card';
 
 const BlogPost = (props) => {
     return (
-        <div>BlogPost</div>
+           <div className="blogPostContainer">
+               <Card>
+                   <div className="blogHeader">
+                        <span className="blogCategory">Featured</span>
+                        <h1 className="postTitle">Fitness Mantra to Live Fit Life</h1>
+                        <span className="postedBy">posted on July 21,2016 by Sora Blogging Tips</span>
+                   </div>
+                   <div className="postImageContainer">
+                     <img src={require("../../blogPostImages/memories-from.jpg")} alt="post Image" />
+                   </div>
+               </Card>
+           </div>
     );
 }
 export default BlogPost;
