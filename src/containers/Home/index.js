0beb73ae190f8b9376443cbf66e7fc617b94fd91
Pankhuri <pankhuritrikha@gmail.com>
2020-06-12 20:25:45 +0530

    Setting of footer

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './style.css';
+import RecentPosts from './RecentPosts';
+import Layout from '../../components/Layout';
 
 const Home = props => {
     return (
         <div>
            Home
+           <Layout>
+                    <RecentPosts style={{width: '70%'}}/>
+            </Layout>
         </div>
     );
 }
