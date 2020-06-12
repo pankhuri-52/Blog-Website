57114442ed6a755f8439ba47d924910f30fa3f66
Pankhuri <pankhuritrikha@gmail.com>
2020-06-12 13:32:03 +0530

    Side Bar is completed

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,9 +4,31 @@ import Card from '../UI/Card';
 
 const Sidebar = (props) => {
    return (
-     <Card width='27%'>
-       Sidebar
-     </Card>
+     <div className="sidebarContainer">
+        <Card style={{marginBottom : '20px', padding : '20px', boxSizing : 'border-box'}}>
+          <div className="cardHeader">
+            <span>About Us</span>
+          </div>
+          <div className="profileImageContainer">
+              <img src={require("../../blogPostImages/mine.jpg")} alt="about me" />
+          </div>
+          <div className="cardBody">
+            <p className="personalBio">My name is Pankhuri Trikha. I am a software developer. I am good in Front-end Development.</p>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="cardHeader">
+            <span>Social Network</span>
+          </div>
+        </Card>
+
+        <Card>
+        <div className="cardHeader">
+            <span>Recent Posts</span>
+          </div>
+        </Card>
+     </div>
    );
 }
 export default Sidebar;
